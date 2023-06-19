8af23b75f18c379e8b8a152f85b5ced54b00cac0
NicChr <nicolaos.christofidis@gmail.com>
2023-06-19 15:35:52 +0100

    CMD check.

--- R/timeplyr-package.R
+++ R/timeplyr-package.R
@@ -1,8 +1,5 @@
 ## usethis namespace: start
 #' @useDynLib timeplyr, .registration = TRUE
-## usethis namespace: end
-NULL
-## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
 NULL
