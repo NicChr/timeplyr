5872af1da69dccebbfcf88162cb5afa68b702bef
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2023-09-15 11:18:24 +0100

    Updated documentation.

---
+++ R/timeplyr.R
@@ -0,0 +1,16 @@
+#' @description
+#' A framework for handling raw date & datetime data \cr
+#' using tidy best-practices from the tidyverse, the efficiency of data.table,
+#' and the speed of collapse.
+#'
+#' You can learn more about the tidyverse,
+#' data.table and collapse using the links below
+#'
+#' \href{https://www.tidyverse.org/learn/}{tidyverse}
+#'
+#' \href{https://cran.r-project.org/web/packages/data.table/vignettes/datatable-intro.html}{data.table}
+#'
+#' \href{https://sebkrantz.github.io/collapse/articles/collapse_intro.html}{collapse}
+#'
+#'
+"_PACKAGE"
