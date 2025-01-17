846487f4359d16e9aee515d01b4c57eed8cc0778
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2025-01-17 09:45:15 +0000

    Small update.

--- R/missing_dates.R
+++ R/missing_dates.R
@@ -3,27 +3,17 @@
 #' @param x A date or datetime vector, or a data frame.
 #'
 #' @returns
-#' A date vector if x is a vector, or a list if x is a `data.frame`.
+#' A `Date` vector.
 #'
-#' @rdname missing_dates
 #' @export
 missing_dates <- function(x){
-  UseMethod("missing_dates")
-}
-#' @export
-missing_dates.default <- function(x){
   check_is_time(x)
   d <- lubridate::as_date(collapse::funique(x))
   d_seq <- time_grid(d, new_timespan("days", 1))
   cheapr::setdiff_(d_seq, d)
 }
-#' @rdname missing_dates
 #' @export
 n_missing_dates <- function(x){
-  UseMethod("n_missing_dates")
-}
-#' @export
-n_missing_dates.default <- function(x){
   check_is_time(x)
   time_num_gaps(
     lubridate::as_date(
